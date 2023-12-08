17454077496cb5b2f057bc922fc8ffefd340cf25
RITIK KUMAR CHAUDHARY <119487018+prisoner79@users.noreply.github.com>
2023-12-08 13:21:17 +0530

    first

---
+++ cinematrix/src/FooterBlock.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./FooterBlock.css";
+
+const FooterBlock = () => {
+  return (
+
+<div className="fd">
+
+<p>Copyright © 2023. Created by ♥ _RC.dev_ ♥</p>
+<div  className="hr_line" >
+
+</div>
+<div className="footer_content" >
+
+    <span> Contact Us</span> | 
+    <span> Request Us</span> | 
+    <span> About Us</span> | 
+    <span> Sitemap</span> 
+    
+</div>
+
+
+
+</div>
+
+
+
+
+  )
+};
+
+export default FooterBlock;
